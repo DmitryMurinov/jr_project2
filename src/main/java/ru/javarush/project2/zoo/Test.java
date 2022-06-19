a25564ebd46d3017b499662812039bd225ca5c26
DmitryMurinov <bRuswezUfewruD4uHe>
2022-06-19 16:19:04 +0300

    Removing project from master to recreate in new branch as Gradle project.

--- src/main/java/ru/javarush/project2/zoo/Test.java
+++
@@ -1,4 +0,0 @@
-package ru.javarush.project2.zoo;
-
-public class Test {
-}
--- src/main/java/ru/javarush/project2/zoo/model/Plant.java
+++
@@ -1,8 +0,0 @@
-package ru.javarush.project2.zoo.model;
-
-public class Plant extends TileItem {
-
-    public Plant(ItemProperties value) {
-        super(value);
-    }
-}
